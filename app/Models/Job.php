f15eceadeb55feb075283f83a4ac2033e581c997
santhiagosilvaa <santhiagosilvaa@gmail.com>
2024-07-23 14:49:31 -0300

    aula 009

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,38 +1,11 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);      
-        
-        if (! $job) {
-            abort(404);
-        }
+class Job extends Model {
+    protected $table = 'job_listings_tables';
 
-        return $job;
-    }
+    protected $fillable = ['title', 'salaray'];
 }
