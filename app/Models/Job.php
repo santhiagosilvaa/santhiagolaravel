8df04855768f22bfa84a895bd48dc42b476c20b9
santhiagosilvaa <santhiagosilvaa@gmail.com>
2024-07-26 14:07:46 -0300

    aula 12

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,9 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
 
 class Job extends Model {
+    use HasFactory;
+    
     protected $table = 'job_listings_tables';
 
-    protected $fillable = ['title', 'salaray'];
+    protected $fillable = ['title', 'salary'];
+
+    public function employed()
+    {
+        return $this->belongsTo((Employer::class));
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listings_tables_id");
+    }
 }
